0b73f5ff51b3d7373d99d219403659ed5a521cb7
Muhammad Ismail <104766223+Connoisseur-Cube@users.noreply.github.com>
2023-01-02 23:27:16 +0500

    finalized connect class

--- src/main/java/housesociety/ConnectToMySQL.java
+++ src/main/java/housesociety/ConnectToMySQL.java
@@ -4,9 +4,18 @@ import java.sql.*;
 
 public class ConnectToMySQL {
 
-    private final String user = "root";
-    private final String password = "1234";
-    private final String url = "jdbc:mysql://localhost:3306/HousingSociety?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    
+/*----------------------------------------------------------------------------------------
+    Ismail here. I turned connection and statement into static because I do not want more instances of these.
+    So even if we create a new instance of the clase, The connection and statement simply get overrided, or "Re-connected"
+    Piling up multiple instances of these is something we should avoid.
+    We can create an instance of this class anywhere without the worry of duplication.
+    I also do not see a reason to have multiple instances of strings that do not change
+-----------------------------------------------------------------------------------------    */    
+    
+    private final static String user = "root";
+    private final static String password = "1234";
+    private final static String url = "jdbc:mysql://localhost:3306/HousingSociety?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private static Connection conn;
     private static Statement s;
     
@@ -25,7 +34,7 @@ public class ConnectToMySQL {
         return s;
     }
     
-    
+    // This function will be called when the application closes
     void close() throws SQLException{
         s.close();
         conn.close();
--- src/main/java/housesociety/HouseSociety.java
+++ src/main/java/housesociety/HouseSociety.java
@@ -2,29 +2,23 @@ package housesociety;
 
 import java.sql.*;
 
-
-
 public class HouseSociety {
 
+//------------------------------------------------------------------------------    
+    
     public static void main(String[] args) {
         
         try {
             ConnectToMySQL instance = new ConnectToMySQL();
             
-            Statement s = instance.getState();
-            ResultSet rs = s.executeQuery("Select * from tenant;");
-            
-            while(rs.next()){
-                System.out.println(rs.getInt("T_ID"));
-                System.out.println(rs.getString("T_Name"));
-                System.out.println(rs.getString("T_Email"));
-                System.out.println(rs.getInt("House_ID"));
-            }
-            
-        } catch (Exception ex) {
+        }           
+        catch (Exception ex) {
             ex.getStackTrace();
-        }
-        
+        }  
 
     }
+//------------------------------------------------------------------------------
+    
+    
+       
 }
